207b230e82c2605bc4c6564d4f0eb51bb4f44f20
Roquv13 <czarekolszewski99@gmail.com>
2024-05-30 12:57:00 +0200

    Add grid to calc

--- src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
+++ src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
@@ -2,7 +2,9 @@ package pl.roquv.calculatorjavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +15,12 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 600);
         stage.setTitle("JavaFX Calculator");
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+
         stage.setScene(scene);
         stage.show();
     }
