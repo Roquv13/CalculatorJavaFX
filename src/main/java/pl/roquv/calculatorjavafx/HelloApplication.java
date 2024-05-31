eaffc741423f8c66aae99f1928a61f2cf2ee9c1b
Roquv13 <czarekolszewski99@gmail.com>
2024-05-31 13:01:05 +0200

    Add fields and label to grid

--- src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
+++ src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,7 +15,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+//        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
         stage.setTitle("JavaFX Calculator");
 
         GridPane grid = new GridPane();
@@ -21,6 +23,13 @@ public class HelloApplication extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(10, 10, 10, 10));
 
+        TextField num1Field = new TextField();
+        TextField num2Field = new TextField();
+        Label resultLabel = new Label("Result:");
+
+        grid.getChildren().addAll(num1Field, num2Field, resultLabel);
+
+        Scene scene = new Scene(grid, 400, 600);
         stage.setScene(scene);
         stage.show();
     }
