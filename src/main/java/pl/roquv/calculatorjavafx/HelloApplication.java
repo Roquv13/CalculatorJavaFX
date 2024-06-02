ed1177daf54a114ec404a92af5b361fe34232dae
Roquv13 <czarekolszewski99@gmail.com>
2024-06-02 14:27:35 +0200

    Button for addition numbers

--- src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
+++ src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
@@ -1,9 +1,12 @@
 package pl.roquv.calculatorjavafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -12,6 +15,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private TextField num1Field;
+    private TextField num2Field;
+    private Label resultLabel;
+
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -23,23 +34,36 @@ public class HelloApplication extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(10, 10, 10, 10));
 
-        TextField num1Field = new TextField();
+        num1Field = new TextField();
         GridPane.setConstraints(num1Field, 0, 0);
 
-        TextField num2Field = new TextField();
+        num2Field = new TextField();
         GridPane.setConstraints(num2Field, 0, 1);
 
-        Label resultLabel = new Label("Result:");
+        resultLabel = new Label("Result:");
         GridPane.setConstraints(resultLabel, 0, 2);
 
-        grid.getChildren().addAll(num1Field, num2Field, resultLabel);
+        Button addButton = new Button("+");
+        addButton.setOnAction(this::add);
+        GridPane.setConstraints(addButton, 1, 0);
+
+        grid.getChildren().addAll(num1Field, num2Field, resultLabel, addButton);
 
         Scene scene = new Scene(grid, 400, 600);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void add(ActionEvent actionEvent) {
+        String num1Text = num1Field.getText();
+        String num2Text = num2Field.getText();
+
+        double num1 = Double.parseDouble(num1Text);
+        double num2 = Double.parseDouble(num2Text);
+        double result = 0.0;
+
+        result = num1 + num2;
+
+        resultLabel.setText("Result: " + result);
     }
 }
