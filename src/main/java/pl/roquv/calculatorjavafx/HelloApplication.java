59d6e01ce95067292fd65e34c80021941213699e
Roquv13 <czarekolszewski99@gmail.com>
2024-06-07 15:35:49 +0200

    Add switch cases for multiply and divide

--- src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
+++ src/main/java/pl/roquv/calculatorjavafx/HelloApplication.java
@@ -78,6 +78,10 @@ public class HelloApplication extends Application {
                 add(num1, num2);
             case '-':
                 subtract(num1, num2);
+            case '*':
+                multiply(num1, num2);
+            case '/':
+                divide(num1, num2);
             default:
                 break;
         }
