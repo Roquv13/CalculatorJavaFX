a21857643f66e40d0bff9a8d63d15d2b56059727
Roquv13 <czarekolszewski99@gmail.com>
2024-06-30 18:59:11 +0200

    Change window icon

--- src/main/java/pl/roquv/calculatorjavafx/Main.java
+++ src/main/java/pl/roquv/calculatorjavafx/Main.java
@@ -3,7 +3,10 @@ package pl.roquv.calculatorjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
@@ -13,6 +16,11 @@ public class Main extends Application {
 
         stage.setTitle("Calculator");
         stage.setScene(new Scene(root.load(), 450, 600));
+
+        // Load icon
+        String iconPath = Main.class.getResource("icon.png").toExternalForm();
+        stage.getIcons().add(new Image(iconPath));
+
         stage.show();
     }
 
