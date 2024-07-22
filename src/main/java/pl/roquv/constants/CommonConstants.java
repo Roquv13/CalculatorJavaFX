fcb1e2d3d6189f4ced180eaeaa83fe171e7874c3
Roquv13 <czarekolszewski99@gmail.com>
2024-07-22 15:29:32 +0200

    Add unary operators as constants

--- src/main/java/pl/roquv/constants/CommonConstants.java
+++ src/main/java/pl/roquv/constants/CommonConstants.java
@@ -1,4 +1,10 @@
 package pl.roquv.constants;
 
 public class CommonConstants {
+    // Unary operators
+    public static final String OPERATOR_PERCENT = "%";
+    public static final String OPERATOR_RECIPROCAL = "1/x";
+    public static final String OPERATOR_SQUARE = "x^2";
+    public static final String OPERATOR_SQRT = "√x";
+    public static final String OPERATOR_NEGATE = "+/-";
 }
